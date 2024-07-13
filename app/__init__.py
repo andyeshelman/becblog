87d40b92cbc30f00e4c596890e56ffcefc76e4ae
andyeshelman <andy.eshelman@gmail.com>
2024-07-13 19:05:09 -0400

    add sqlite fallback db

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ import os
 from app.modules import db, migrate, cache, limiter, swaggerui_blueprint
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'sqlite:///testing.db'
 app.json.sort_keys = False
 
 db.init_app(app)
